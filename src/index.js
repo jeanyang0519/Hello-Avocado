b386c83632f13442a6fdc2717713aed68e0e8c32
Jean <sy2803@tc.columbia.edu>
2020-01-09 09:26:13 -0500

    separate the files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 import './css/home.scss'
-import '../lib/volume'
 
-const button = document.getElementById('clickme')
-button.onclick = handdleClick;
 
-function handdleClick() {
-    remove()
-    update(button.dataset.California)
-} 
-
-const button2 = document.getElementById('clickme-2')
-// button2.onclick = handdleClick2;
-// debugger
-// function handdleClick2() {
-//     // debugger
-//     remove()
-//     update(button2.dataset.Chile)
-
-button2.addEventListener('click', () => update('Chile'))
-// }
 
